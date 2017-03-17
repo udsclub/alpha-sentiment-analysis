1fb53277670ea0929db7b9b5bcd2407f99d2885b
vitaliyradchenko <radchenko-v.i.p@mail.ru>
2017-03-17 14:30:57 +0200

    added guide

--- amazonTv/README.md
+++ amazonTv/README.md
@@ -7,3 +7,8 @@
 LSTM&m – whole dataset; split VALIDATION_SPLIT = 0.06552, RANDOM_SEED = 42; masking(0)
 
 Test – train_test_split(data, labels, test_size=0.1, random_state=42, stratify=labels)
+
+## Reproduce results
+1. Prepare amazon dataset – download and put amazon dataset in "data" folder and run prepare_amazon_data.py
+2. Download google word2vec pretrained vectors https://github.com/3Top/word2vec-api and put in "embeddings" folder
+3. Run lstm_with_prepr.py
--- amazonTv/scripts/lstm_with_prepr.py
+++ amazonTv/scripts/lstm_with_prepr.py
@@ -15,11 +15,10 @@ from keras.callbacks import ModelCheckpoint, TensorBoard, EarlyStopping
 from keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
 
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
 BASE_DIR = '../'
-EMBEDDING_DIR = BASE_DIR + 'embeddings/'  # http://nlp.stanford.edu/projects/glove/ pretrained vectors
-EMBEDDING_FILE = "GoogleNews-vectors-negative300.bin"
+EMBEDDING_DIR = BASE_DIR + 'embeddings/'
+EMBEDDING_FILE = "GoogleNews-vectors-negative300.bin" # https://github.com/3Top/word2vec-api
 TEXT_DATA_DIR = BASE_DIR + 'data/'
 TEXT_DATA_FILE = "train_movies.csv"
 LR = 0.001
@@ -30,11 +29,13 @@ NAME = "amazon_" + suffix
 BOTH_SIDES = False
 print(suffix)
 
+
 MAX_NB_WORDS = 50000
 EMBEDDING_DIM = 300
 MAX_SEQUENCE_LENGTH = 110
-VALIDATION_SPLIT = 0.1
+VALIDATION_SPLIT = 0.06552
 RANDOM_SEED = 42
+
 
 negatives = {
     "didn't": "didn_`_t",
@@ -44,7 +45,7 @@ negatives = {
     "doesn't": "doesn_`_t",
     "wasn't": "wasn_`_t",
     "weren't": "weren_`_t",
-    "shouldn't": "shouldn_`_t",
+    "shouldn't":"shouldn_`_t",
     "isn't": "isn_`_t",
     "aren't": "aren_`_t",
 }
@@ -58,19 +59,18 @@ def preprocess(text):
     return text
 
 
-def load_data(n):
+def load_data():
     df_train = pd.read_csv(os.path.join(TEXT_DATA_DIR, TEXT_DATA_FILE))
-    df_train, _ = train_test_split(df_train, train_size=n, random_state=42)
-    train, test = train_test_split(df_train.asin.unique(), test_size=VALIDATION_SPLIT, random_state=42)
+    train, test = train_test_split(df_train.asin.unique(), test_size=0.1, random_state=42)
     train_reviews, labels_train_reviews = df_train.loc[(df_train.asin.isin(train)) & (~pd.isnull(df_train.reviewText)),
                                                        "reviewText"].values, \
                                           df_train.loc[(df_train.asin.isin(train)) & (~pd.isnull(df_train.reviewText)),
                                                        "overall"].values
     test_reviews, labels_test_reviews = df_train.loc[(df_train.asin.isin(test)) & (~pd.isnull(df_train.reviewText)),
                                                      "reviewText"].values, df_train.loc[(df_train.asin.isin(test)) &
-                                                                                        (~pd.isnull(
-                                                                                            df_train.reviewText)),
+                                                                                        (~pd.isnull(df_train.reviewText)),
                                                                                         "overall"].values
+
     for x in range(len(train_reviews)):
         train_reviews[x] = preprocess(train_reviews[x])
     for x in range(len(test_reviews)):
@@ -83,12 +83,22 @@ def transform(tokenizer_object, train, test, l_train, l_test, both_sides=True):
     sequences_train = tokenizer_object.texts_to_sequences(train)  # transform words to its indexes
     sequences_test = tokenizer_object.texts_to_sequences(test)
 
-    word_indexes = tokenizer_object.word_index  # dictionary of word:index
+    word_indexes = tokenizer.word_index  # dictionary of word:index
 
     # transform a list to numpy array with shape (nb_samples, MAX_SEQUENCE_LENGTH)
     # be careful because it takes only last MAX_SEQUENCE_LENGTH words
     train = pad_sequences(sequences_train, maxlen=MAX_SEQUENCE_LENGTH)
     test = pad_sequences(sequences_test, maxlen=MAX_SEQUENCE_LENGTH)
+
+    if both_sides:
+        indexes_train = [n for n, text in enumerate(sequences_train) if len(text) >= 100]
+        indexes_test = [n for n, text in enumerate(sequences_test) if len(text) >= 100]
+        train = np.append(train, pad_sequences(np.array(sequences_train)[indexes_train], maxlen=MAX_SEQUENCE_LENGTH,
+                                               truncating='post'), axis=0)
+        test = np.append(test, pad_sequences(np.array(sequences_test)[indexes_test], maxlen=MAX_SEQUENCE_LENGTH,
+                                             truncating='post'), axis=0)
+        l_train = np.append(l_train, labels_train[indexes_train], axis=0)
+        l_test = np.append(l_test, labels_test[indexes_test], axis=0)
 
     return train, test, word_indexes, l_train, l_test
 
@@ -117,6 +127,7 @@ def prepare_embeddings(word_indexes):
 
 
 def model_initialization(embeddings):
+
     model = Sequential()
     model.add(embeddings)
     model.add(Dropout(0.3))
@@ -128,11 +139,12 @@ def model_initialization(embeddings):
     return model
 
 
-def train_model(optimizer, embeddings, X_train, y_train, X_test, y_test, add_to_name):
-    callback_1 = TensorBoard(log_dir='./logs/logs_{}'.format(NAME + "_" + str(add_to_name)), histogram_freq=0,
+def train_model(optimizer, embeddings, add_to_name):
+
+    callback_1 = TensorBoard(log_dir='./logs/logs_{}'.format(NAME+str(add_to_name)), histogram_freq=0,
                              write_graph=False, write_images=False)
     callback_2 = EarlyStopping(monitor='val_acc', min_delta=0, patience=10, verbose=0, mode='auto')
-    callback_3 = ModelCheckpoint("models/model_{}.hdf5".format(NAME + "_" + str(add_to_name)), monitor='val_acc',
+    callback_3 = ModelCheckpoint("models/model_{}.hdf5".format(NAME+str(add_to_name)), monitor='val_acc',
                                  save_best_only=True, verbose=0)
 
     embedding_layer = Embedding(embeddings.shape[0],
@@ -151,20 +163,19 @@ def train_model(optimizer, embeddings, X_train, y_train, X_test, y_test, add_to_
               callbacks=[callback_1, callback_2, callback_3], verbose=2)
 
 
-def run(n):
-    data_train, labels_train, data_test, labels_test = load_data(n)
-    print(len(data_train), len(data_test))
-    # tokenizer = Tokenizer(nb_words=MAX_NB_WORDS)  # create dictionary of MAX_NB_WORDS, other words will not be used
-    tokenizer = Tokenizer(nb_words=MAX_NB_WORDS, filters='"#$%&()*+-/:;<=>@[\\]^{|}~\t\n,.')
-    tokenizer.fit_on_texts(data_train)
-    six.moves.cPickle.dump(tokenizer, open("../tokenizers/{}_tokenizer_{}".format(NAME, n), "wb"))
+data_train, labels_train, data_test, labels_test = load_data()
+print(len(data_train), len(data_test))
 
-    X_train, X_test, word_index, labels_train, labels_test = transform(tokenizer, data_train, data_test,
-                                                                       labels_train, labels_test, BOTH_SIDES)
-    y_train, y_test = to_categorical(np.asarray(labels_train)), to_categorical(np.asarray(labels_test))
-    embedding_matrix = prepare_embeddings(word_index)
-    train_model(Adam(lr=LR), embedding_matrix, X_train, y_train, X_test, y_test, n)
+# tokenizer = Tokenizer(nb_words=MAX_NB_WORDS)  # create dictionary of MAX_NB_WORDS, other words will not be used
+tokenizer = Tokenizer(nb_words=MAX_NB_WORDS, filters='"#$%&()*+-/:;<=>@[\\]^{|}~\t\n,.')
 
-for i in [100000, 200000, 500000, 1000000]:
-    run(i)
+tokenizer.fit_on_texts(data_train)
+six.moves.cPickle.dump(tokenizer, open("../tokenizers/{}_tokenizer".format(NAME), "wb"))
 
+X_train, X_test, word_index, labels_train, labels_test = transform(tokenizer, data_train, data_test,
+                                                                   labels_train, labels_test, BOTH_SIDES)
+y_train, y_test = to_categorical(np.asarray(labels_train)), to_categorical(np.asarray(labels_test))
+
+embedding_matrix = prepare_embeddings(word_index)
+
+train_model(Adam(lr=LR), embedding_matrix, "base")
